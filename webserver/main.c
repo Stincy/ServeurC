de69274698ce272da6fb0199a1638dfd22a3bb8b
Damien Lepeltier <jean.dupont@example.com>
2015-01-28 10:21:44 +0100

    je sais pas

--- webserver/main.c
+++ webserver/main.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 #include "socket.h"
 
-int main(){
-	creer_serveur(8000);
-	printf("main ok"); 
+int main(int argc , char **argv){
 
+	/* Arnold Robbins in the LJ of February ’95 , describing RCS */
+	if ( argc > 1 && strcmp ( argv [1] , " - advice " ) == 0) {
+		printf("don't panic \n");
+	return 42;
+	}
+
+
+	creer_serveur(8000);
+
+	printf ( " Need an advice ? \n " );
 	return 0;
 }
